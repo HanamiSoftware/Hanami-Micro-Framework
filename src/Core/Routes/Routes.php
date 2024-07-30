5a914d2c1a0613f69b1b14f557f00974bb855172
FRANCESCO BIAGINI <info@hanamisoftware.com>
2024-07-30 17:32:37 +0200

    Starting Routes Class in Hanami\Core\Routes

---
+++ src/Core/Routes/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hanami\Core\Routes
+{
+	/**
+	 * Routes short summary.
+	 *
+	 * Routes description.
+	 *
+	 * @version 1.0
+	 * @author Francesco
+	 */
+	class Routes
+	{
+		public static function add(string $method, string $path, callable $callback): void{
+            
+        }
+	}
+}
